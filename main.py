f8ce5820037c8d5932fa7a7ba2a9b416b58fef18
itprogrammist <kozlovdmitrij796@gmail.com>
2021-04-03 20:04:03 +0700

    Tasks # 1,2,3,5

--- main.py
+++ main.py
@@ -1,39 +1,69 @@
 import conf
 import discord
 
-client = discord.Client()
+# Настраиваем расширенный доступ Intents
+intense = discord.Intents.default()
+intense.members = True
+
+# Создаём подключение бота
+client = discord.Client(intents=intense)
 
 
 
 
-@client.event # Функция сработает только в тот момент, когда на неё придёт запрос
+@client.event
 async def on_message(message):
-    # Проверка на дурака №1 - Отправитель является самим ботом
+    # <Message 
+    # id=825337927005110293 
+    # channel=<TextChannel id=822806350886207542 name='флудильня' position=0 nsfw=False news=False category_id=822806350886207539> 
+    # type=<MessageType.default: 0> 
+    # author=<Member id=635376536761139211 name='Ulan' discriminator='9113' bot=False nick=None guild=<Guild id=822806350886207538 name='Bots' shard_id=None chunked=False member_count=29>>
+    # flags=<MessageFlags value=0>>
+
+    # Проверка на дурака №1 - Отправлятор является самим ботом
+    if message.author == client.user:
+        return
+    
+    # Проверка на дурака №2 - Отправлятор является другим ботом
     if message.author.bot:
         return
-    # Проверка на дурака №2 - Отправитель является другим ботом
-    if 
 
-    if message.author == client.user:
-        # Изначально сообщение пустое
-
+    # задаём каналы для бота [Канал General]
     if message.channel.id == 825340703084118046:
-        print(message)
-        # Ответить пользователю в формате "hello, {user.name} - your message {user.}"
+        # Изначально, сообщение пустое
         msg = None
-        #Отправляем сообщение в канал channel
+        
+        # Контекс, разбивающийся на два эл-а списка (команду /... и 'само сообщение')
+        ctx = message.content.split(" ", maxsplit = 1) # Максимальное количество
 
         # 1. /hello - просто сообщение
-        # 2. /about_me - сообщение пользователю по его параметру id/name (если есть ник, то добавить "твой ник nick")
-        # 3. /repeat [] - повторить за пользователем
         if message.content == "/hello":
             msg = f'Hello, {message.author.name}. I am {client.user.name}'
 
-        elif message.content == "/about_me":
-            await bot.say("{} is your name".format(message.author.id))
-        
-        # Отправляем сообщение, если оно есть
-        if  msg != None:
+        # 2. /about_me - сообщение пользователю по его параметрам id/name (если есть ник то добавить "твой ник nick")
+        elif message.content == "/about me":
+            msg = f'Hello, {message.author.name}, your id is: {message.author.id}'
+
+        # *3. /repeat [] - повторить за пользователем
+        elif ctx[0] == "/repeat":
+            msg = ctx[1]
+
+        #*4 /get_member
+        # elif ctx[0] == "/get_member":
+        #     name = ctx[1]
+        #     if name == '':
+        #         msg = f'Enter username, please!'
+        #     elif 
+
+        # *5 /get_members
+        elif message.content == '/get_members':
+            msg = ""
+            if message.author.guild.name == 'Bots':
+                for idx, member in list(enumerate(message.author.guild.members)):
+                    msg += f'{idx+1}. {member.name} { f"[{member.nick}]" if member.nick else ""} - {member.id}\n'          
+                        
+        # Отправляем сообщение если оно есть
+        if msg != None:
             await message.channel.send(msg)
 
 
@@ -41,6 +71,3 @@ async def on_message(message):
 
 
 client.run(conf.bot_token)
-
-
-
